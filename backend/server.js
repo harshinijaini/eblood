c153340a5d2df70b7b56ac83852a59871322da5c
Harsh <Harsh@JAGADEESHs-MacBook-Air.local>
2024-04-16 00:30:20 -0400

    Made the app production ready

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
+// import { path } from "path";
+const path = require("path");
 
 const express = require("express");
 const mongoose = require("mongoose");
 const donorRoutes = require("./routes/donors");
 const userRoutes = require("./routes/user");
 const cors = require("cors");
+// const __dirname = path.resolve();
 
 //express app
 const app = express();
@@ -22,6 +25,17 @@ app.use((req, res, next) => {
 
 app.use("/api/donors", donorRoutes);
 app.use("/api/user", userRoutes);
+
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./frontend/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
 
 //connect to db
 mongoose
